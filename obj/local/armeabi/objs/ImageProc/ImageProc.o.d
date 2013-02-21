35fbbfd361511f3acbea90731aaa17fec0e2aeb7
haizhu12345@gmail.com <haizhu12345@gmail.com>
2013-02-21 16:00:02 +0800

    android摄像头 project

---
+++ obj/local/armeabi/objs/ImageProc/ImageProc.o.d
@@ -0,0 +1,217 @@
+/cygdrive/f/simplewebcam/obj/local/armeabi/objs/ImageProc/ImageProc.o: \
+ /cygdrive/f/simplewebcam/jni/ImageProc.c /cygdrive/f/simplewebcam/jni/ImageProc.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/jni.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/bitmap.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/string.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/strings.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/memory.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/assert.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/fcntl.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/fcntl.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/fcntl.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/fcntl.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/unistd.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/select.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/time.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/time.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/signal.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/limits.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/signal.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/signal.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/sigcontext.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/siginfo.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/sysconf.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/capability.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/pathconf.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/errno.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/errno.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/errno.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/stat.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/stat.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/endian.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/endian.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/mman.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/mman.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/mman.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/ioctl.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/ioctl.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/ioctl.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/ioctl.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/ioctls.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/termbits.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/ioctl_compat.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/videodev2.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/usbdevice_fs.h
+
+/cygdrive/f/simplewebcam/jni/ImageProc.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/bitmap.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/string.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/fcntl.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/fcntl.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/fcntl.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/unistd.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/select.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/time.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/time.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/signal.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/signal.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/signal.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/sigcontext.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/siginfo.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/sysconf.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/capability.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/pathconf.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/errno.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/errno.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/errno.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/stat.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/stat.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/endian.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/endian.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/mman.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/mman.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/mman.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/ioctl.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/ioctl.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/ioctl.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm-generic/ioctl.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/ioctls.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/asm/termbits.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/sys/ioctl_compat.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/videodev2.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-8/arch-arm/usr/include/linux/usbdevice_fs.h:
